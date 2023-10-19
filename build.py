9390e99bdad978ed0ff88f2d1dd8b9c49c73073c
Brandon Stine <brandon.stine99@gmail.com>
2023-10-19 18:50:58 -0400

    Update build.py

--- build.py
+++ build.py
@@ -5,13 +5,7 @@ from simtk import unit
 import simtk.openmm as omm
 
 def build_by_seq(seq, forcefield):
-    step = 0.22
-
-    # Define a simple geometry for the backbone bead of each amino acid
-    geometry = {'A': [0, 0, 0], 'R': [0, 0, 0], 'N': [0, 0, 0], 'D': [0, 0, 0], 'C': [0, 0, 0],
-                'E': [0, 0, 0], 'Q': [0, 0, 0], 'G': [0, 0, 0], 'H': [0, 0, 0], 'I': [0, 0, 0],
-                'L': [0, 0, 0], 'K': [0, 0, 0], 'M': [0, 0, 0], 'F': [0, 0, 0], 'P': [0, 0, 0],
-                'S': [0, 0, 0], 'T': [0, 0, 0], 'W': [0, 0, 0], 'Y': [0, 0, 0], 'V': [0, 0, 0]}
+    step = 0.22  # distance between consecutive backbone beads
 
     topo = omm.app.Topology()
     chain = topo.addChain('X')
@@ -22,7 +16,8 @@ def build_by_seq(seq, forcefield):
     for i, resSymbol in enumerate(seq):
         res = topo.addResidue(resSymbol, chain)
         atoms.append(topo.addAtom('B', None, res))  # 'B' represents backbone bead
-        positions.append(geometry[resSymbol])
+        z_position = i * step  # Update the z-position based on the step and sequence index
+        positions.append([0, 0, z_position] * unit.nanometers)  # Convert position to nanometers unit
         if i > 0:  # Add bond between consecutive backbone beads
             topo.addBond(atoms[i-1], atoms[i])
 
